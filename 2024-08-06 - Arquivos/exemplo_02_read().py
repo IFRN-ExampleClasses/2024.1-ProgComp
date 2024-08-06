ff91c6c4339b1224212e04e0ba51d1fca0895d7e
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-08-06 18:00:32 -0300

    Arquivos
    Operações com arquivos: OPEN() / WRITE() / READ() / CLOSE()

---
+++ 2024-08-06 - Arquivos/exemplo_02_read().py
@@ -0,0 +1,16 @@
+import os
+
+# Obtendo o diretorio onde os arquivos estão
+dirArquivos = os.path.abspath(__file__)
+dirArquivos = os.path.dirname(dirArquivos)
+
+# Abrindo o arquivo no modo somente leitura
+arqEntrada = open(dirArquivos + '\\lista_uf_capitais.txt','r', encoding='utf-8')
+
+# Lendo o conteúdo do arquivo
+conteudo = arqEntrada.read()
+
+print(conteudo)
+
+# Fechando o arquivo
+arqEntrada.close()
---
+++ 2024-08-06 - Arquivos/exemplo_04_readline().py
@@ -0,0 +1,28 @@
+import os
+
+# Obtendo o diretorio onde os arquivos estão
+dirArquivos = os.path.abspath(__file__)
+dirArquivos = os.path.dirname(dirArquivos)
+
+# Abrindo o arquivo no modo somente leitura
+arqEntrada = open(dirArquivos + '\\lista_uf.txt','r', encoding='utf-8')
+
+lstUF = list()
+
+while True:
+    # Lendo o conteúdo do arquivo
+    linha = arqEntrada.readline()
+    if linha[-1:] == '\n': linha = linha[:-1]
+
+    # Interrompendo a leitura quando chegar ao final do arquivo
+    if not linha: break
+    
+    # Adicionando a linha lida a uma lista
+    lstUF.append(linha)
+
+# Fechando o arquivo
+arqEntrada.close()
+
+# Exibindo os dados lidos
+print(lstUF)
+
